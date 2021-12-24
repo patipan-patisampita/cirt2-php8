a9bd004e7730d9e4ebfc0152348d9747ce292cd8
patipan-patisampita <patipan.patisampita@gmail.com>
2021-12-24 11:26:32 +0700

    initial commit

--- index.php
+++ index.php
@@ -2,7 +2,40 @@
 
 <?php include("includes/navbar.php"); ?>
 
-<?php include("includes/slider.php");?>
+<?php include("includes/slider.php"); ?>
+
+<!-- About as -->
+<section class="section">
+    <div class="container-fluid mt-3">
+        <div class="row">
+
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body shadow">
+                        <div class="card-text">
+                            เกี่ยวกับเว็บไซต์ นายมาร์ก
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body shadow">
+                        <div class="card-text">
+                        ประวัติส่วนตัว
+                        </div>
+                        <ul type="sqare">
+                            <li>สำเร็จการศึกษ ประถมศึกษาชั้นปีที่ 6 ณ โรงเรียน บ้านหนองบอน ปี พ.ศ.2523</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+<!-- End About as -->
 
 <?php include("includes/scripts.php"); ?>
 
