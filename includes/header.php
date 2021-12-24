e91864a59f4bcd3c493a2e011b41ccd445d88063
patipan-patisampita <patipan.patisampita@gmail.com>
2021-12-24 10:53:17 +0700

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>เว็บนายมาร์ก</title>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/customer.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
