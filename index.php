52de6e983dbb23db66b41a4ca4717b404da29ef7
patipan-patisampita <patipan.patisampita@gmail.com>
2021-12-24 09:55:43 +0700

    initial commit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <head>
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #359;">
             <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">นายมาร์ก</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -24,22 +24,25 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
+
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                            เกี่ยวกับเรา
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">ประวัติส่วนตัว</a></li>
+                                <li><a class="dropdown-item" href="#">เกี่ยวกับเรา</a></li>
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">หน้าที่รับผิดชอบ</a></li>
                             </ul>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ทีมงานของเรา</a>
+                        </li>
+                        
                         <li class="nav-item">
                             <a class="nav-link disabled">Disabled</a>
                         </li>
